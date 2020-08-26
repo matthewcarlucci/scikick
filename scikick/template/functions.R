6b015c03160581c5d8345f01d0cd6a4eb17f403a
Tadas Bareikis <tadasbar@gmail.com>
2020-08-26 08:51:47 +0300

    Add commit sha column to the "List of Changes" in the footer

--- scikick/template/functions.R
+++ scikick/template/functions.R
@@ -1,7 +1,7 @@
 scikick_get_gitlog = function(input=NULL){
 
-    default_gitlog = data.frame(author="user", date=Sys.Date(),
-        summary="no git history", stringsAsFactors=FALSE)
+    default_gitlog = data.frame(author="user", summary="no git history",
+        date=Sys.Date(), sha="00000000", stringsAsFactors=FALSE)
 
     gitlog = tryCatch({
         suppressWarnings({git2r::commits(git2r::repository("."), path=input)})
@@ -12,9 +12,10 @@ scikick_get_gitlog = function(input=NULL){
         "is earlier than 0.27, page will lack git logs")
     if(length(gitlog) > 0) {
         gitlog = tryCatch(
-            expr={data.frame(author = sapply(gitlog, function(x) x$author$name),
+            expr={data.frame(author=sapply(gitlog, function(x) x$author$name),
                 summary=sapply(gitlog, function(x) x$summary),
                 date=sapply(gitlog, function(x) as.character(x$author$when)),
+                sha=sapply(gitlog, function(x) strtrim(x$sha, 8)),
                 stringsAsFactors=FALSE)},
             error=function(e) {
                 if(packageVersion("git2r") < "0.27"){
